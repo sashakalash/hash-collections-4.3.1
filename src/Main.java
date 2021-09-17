d7607d98244a51a687e3476785275136a029d1f4
Aleksandr Koloshtivin <aykolosh@mts.ru>
2021-09-17 18:25:12 +0300

    Замечания исправлены

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
+    public static PhoneContacts phoneContacts = new PhoneContacts();
 
     public static void main(String[] args) {
         System.out.println("Введите название группы контактов:");
@@ -17,13 +19,19 @@ public class Main {
             if ("нет".equals(input)) {
                 break;
             }
-            PhoneContacts.addGroup(input);
+            if (phoneContacts.getContacts().containsKey(input)) {
+                System.out.println("Группа с таким названием уже существует! Задайте другое название");
+                return;
+            }
+            phoneContacts.addGroup(input);
             System.out.println("Создать еще одну группу (введите название или введите нет)?");
         }
     }
 
     public static void addContactToGroup() {
         while (true) {
+            HashMap<String, ArrayList<Contact>> contacts =  phoneContacts.getContacts();
+
             System.out.println("Создать контакт (введите наименование и его номер или введите нет)?");
             String input = scanner.nextLine();
             if ("нет".equals(input)) {
@@ -33,8 +41,14 @@ public class Main {
 
             System.out.println("Укажите группы контакта через пробел");
             input = scanner.nextLine();
-            for (String group : input.split(" ")) {
-                PhoneContacts.addContactToGroup(contact, group);
+            for (String groupTitle : input.split(" ")) {
+                if (contacts.containsKey(groupTitle)) {
+                    phoneContacts.addContactToExistingGroup(contact,groupTitle);
+                    System.out.println(contact.getName() + " успешно добавлен в группу '" + groupTitle + "'");
+                } else {
+                    phoneContacts.addContactToNewGroup(contact,groupTitle);
+                    System.out.println("Группы " + groupTitle + " не существует! Создана новая группа. Контакт добавлен в новую группу");
+                }
             }
         }
 
@@ -48,6 +62,17 @@ public class Main {
 
     public static void showContacts() {
         System.out.println("Группы в справочнике:");
-        PhoneContacts.getContacts();
+        phoneContacts.getContacts().entrySet().forEach(entry -> {
+            String group = entry.getKey();
+            ArrayList<Contact> contacts = entry.getValue();
+            System.out.println("- " + group + ":");
+            if (contacts.size() > 0) {
+                for (Contact contact : contacts) {
+                    System.out.println("Имя: " + contact.getName() + ", Телефон: " + contact.getPhone());
+                }
+            } else {
+                System.out.println("Пока контактов нет");
+            }
+        });
     }
 }
